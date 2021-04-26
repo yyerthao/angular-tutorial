5a044ca351d223a4dee1ca0107124c3d41fb0ed6
Yer Thao <maivyerthao@gmail.com>
2021-04-26 13:16:32 -0500

    Variable declaration notes

---
+++ ts-hello/main.js
@@ -0,0 +1,29 @@
+// function log(message){
+//     console.log(message);
+// }
+// var message = 'Hello World';
+// log(message);
+// go to terminal 
+//  tsc main.ts
+//  node main.js
+// should print out Hello World in terminal
+// ------------------------ Variable declarations ------------------------
+// var number = 1;
+// let count = 2:
+// ------------------------------------------------------------------
+// function doSomething(){
+//     for(let i = 0; i < 5; i++){
+//         console.log(i);
+//     }
+//     console.log('Finally', + i);
+// }
+// doSomething();
+// IN TERMINAL
+// tsc main.ts | node main.js
+// ------------------------------------------------------------------
+var a;
+a = 1;
+a = true;
+a = 'a';
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
---
+++ ts-hello/main.ts
@@ -0,0 +1,60 @@
+
+
+// function log(message){
+//     console.log(message);
+// }
+
+// var message = 'Hello World';
+
+// log(message);
+// go to terminal 
+//  tsc main.ts
+//  node main.js
+// should print out Hello World in terminal
+
+// ------------------------ Variable declarations ------------------------
+
+// var number = 1;
+// let count = 2:
+
+// ------------------------------------------------------------------
+
+// function doSomething(){
+//     for(let i = 0; i < 5; i++){
+//         console.log(i);
+//     }
+//     console.log('Finally', + i);
+// }
+
+// doSomething();
+
+// IN TERMINAL
+// tsc main.ts | node main.js
+
+
+// ------------------------------------------------------------------
+
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[] = [1, 2, 3]; // can set to number array 
+let f: any[] = [1, true, 'a', false]; // can set to any array with multiple values
+
+// 
+const ColorRed = 0;
+const ColorGreen = 1;
+const ColorBlue = 2;
+
+// enum is a constance 
+// how to declare an enum
+enum Color {Red , Green, Blue};
+let backgroundColor = Color.Red;
+
+// ------------------------------------------------------------------
+
+
+
+
+
+// ------------------------------------------------------------------
